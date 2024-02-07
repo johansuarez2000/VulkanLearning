d4236c063cf6debebda04d9516b7109254f26642
Sebastian <jssuarezse@unal.edu.co>
2024-02-07 09:16:34 +0100

    first commit

---
+++ HelloTriangle/compile.sh
@@ -0,0 +1,2 @@
+/home/az03412/Downloads/vulkansdk-linux-x86_64-1.3.275.0/1.3.275.0/x86_64/bin/glslc shaders/shader.vert -o vert.spv
+/home/az03412/Downloads/vulkansdk-linux-x86_64-1.3.275.0/1.3.275.0/x86_64/bin/glslc shaders/shader.frag -o frag.spv
