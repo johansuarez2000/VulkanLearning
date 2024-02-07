c5bbcd370666487ac5987c2ebeb228748b418549
Sebastian <jssuarezse@unal.edu.co>
2024-02-07 15:25:41 +0100

    sss

--- HelloTriangle/compile.sh
+++
@@ -1,2 +0,0 @@
-/home/az03412/Downloads/vulkansdk-linux-x86_64-1.3.275.0/1.3.275.0/x86_64/bin/glslc shaders/shader.vert -o vert.spv
-/home/az03412/Downloads/vulkansdk-linux-x86_64-1.3.275.0/1.3.275.0/x86_64/bin/glslc shaders/shader.frag -o frag.spv
---
+++ HelloTriangle/shaders/compile.sh
@@ -0,0 +1,2 @@
+/home/az03412/Downloads/vulkansdk-linux-x86_64-1.3.275.0/1.3.275.0/x86_64/bin/glslc shader.vert -o vert.spv
+/home/az03412/Downloads/vulkansdk-linux-x86_64-1.3.275.0/1.3.275.0/x86_64/bin/glslc shader.frag -o frag.spv
